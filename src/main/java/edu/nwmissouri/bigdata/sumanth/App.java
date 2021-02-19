b4e6249b383bb9f4925010794a950ef4d9eb3b7f
sumanthreddy1233 <sumanthreddy.naidu@gmail.com>
2021-02-19 13:23:28 -0600

    added src and pom.xml

---
+++ src/main/java/edu/nwmissouri/bigdata/sumanth/App.java
@@ -0,0 +1,13 @@
+package edu.nwmissouri.bigdata.sumanth;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello this is my first project in maven" );
+    }
+}
